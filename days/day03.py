a0b5e53c458e61cfbf0fec5d906fd3f1edfd20d1
Rik van Toor <rik.van.toor@gmail.com>
2022-12-03 09:25:42 +0100

    Day 3

--- days/day03.py
+++ days/day03.py
@@ -1,12 +1,25 @@
 import utils
 
 def parse(input):
-  return input
+  return [list(map(to_prio, l)) for l in input.strip().split('\n')]
+
+def to_prio(c):
+  if c >= 'a':
+    return ord(c) - ord('a') + 1
+  else:
+    return ord(c) - ord('A') + 27
+
+def halves(l):
+  half_index = round(len(l)/2)
+  return (l[0:half_index],l[half_index:])
 
 def part_1(input):
-  print('Part 1 not implemented yet')
-  return 0
+  h = map(halves, input)
+  res = [sum(set([x for x in h1 if x in h2])) for (h1, h2) in h]
+  return sum(res)
 
 def part_2(input):
-  print('Part 2 not implemented yet')
-  return 0
+  chunks = [input[i:i+3] for i in range(0, len(input), 3)]
+  intersections = [sum(set([x for x in c1 if x in c2 if x in c3]))
+                   for [c1, c2, c3] in chunks]
+  return sum(intersections)
