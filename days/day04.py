784170bf2ceaed9f0c20c8c489e458110ee9aa6d
Rik van Toor <rik.van.toor@gmail.com>
2022-12-04 09:24:18 +0100

    Day 4

--- days/day04.py
+++ days/day04.py
@@ -1,12 +1,19 @@
 import utils
 
 def parse(input):
-  return input
+  return [[(int(r.split('-')[0]), int(r.split('-')[1])) for r in l.split(',')]
+          for l in input.strip().split('\n')]
 
 def part_1(input):
-  print('Part 1 not implemented yet')
-  return 0
+  counter = 0
+  for [(p1_min, p1_max), (p2_min, p2_max)] in input:
+    if (p1_min <= p2_min and p1_max >= p2_max) or (p2_min <= p1_min and p2_max >= p1_max):
+      counter += 1
+  return counter
 
 def part_2(input):
-  print('Part 2 not implemented yet')
-  return 0
+  counter = 0
+  for [(p1_min, p1_max), (p2_min, p2_max)] in input:
+    if (p1_min <= p2_min and p1_max >= p2_min) or (p2_min <= p1_min and p2_max >= p1_min):
+      counter += 1
+  return counter
