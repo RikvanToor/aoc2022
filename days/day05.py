4a241983676ff8fe7dddb81624a21b97a4145c81
Rik van Toor <rik.van.toor@gmail.com>
2022-12-05 08:48:40 +0100

    Day 5

--- days/day05.py
+++ days/day05.py
@@ -1,12 +1,40 @@
 import utils
+import re
 
 def parse(input):
-  return input
+  (stacks_txt, proc) = input.rstrip().split('\n\n')
+  stacks_lines = stacks_txt.split('\n')
+  nr_stacks = int(stacks_lines[-1].split()[-1])
+  stacks = [[] for _ in range(nr_stacks)]
+  for l in reversed(stacks_lines[0:-1]):
+    for i in range(nr_stacks):
+      index = (i + 1) * 4 - 3
+      if len(l) >= index:
+        c = l[index]
+        if c != ' ':
+          stacks[i].append(c)
+
+  p = re.compile('\d+')
+  moves = [list(map(int, p.findall(l))) for l in proc.split('\n')]
+
+  return (stacks, moves)
 
 def part_1(input):
-  print('Part 1 not implemented yet')
-  return 0
+  (stacks, moves) = input
+  for [quantity, fr, to] in moves:
+    for i in range(quantity):
+      c = stacks[fr-1].pop()
+      stacks[to-1].append(c)
+
+  return ''.join([s[-1] for s in stacks])
 
 def part_2(input):
-  print('Part 2 not implemented yet')
-  return 0
+  (stacks, moves) = input
+  for [quantity, fr, to] in moves:
+    cs = stacks[fr-1][-quantity:]
+    del stacks[fr-1][-quantity:]
+    stacks[to-1].extend(cs)
+  for s in stacks:
+    if len(s) == 0:
+      s.append(' ')
+  return ''.join([s[-1] for s in stacks])
