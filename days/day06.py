23a7a61e299b82337c836a30d2bffa6ae49cb00d
Rik van Toor <rik.van.toor@gmail.com>
2022-12-06 06:57:13 +0100

    Day 6

--- days/day06.py
+++ days/day06.py
@@ -1,12 +1,17 @@
 import utils
 
 def parse(input):
-  return input
+  return input.strip()
+
+def solve(input, size):
+  for i in range(len(input) - size + 1):
+    slice = input[i:i+size]
+    if len(set(slice)) == size:
+      return i + size
+  return -1
 
 def part_1(input):
-  print('Part 1 not implemented yet')
-  return 0
+  return solve(input, 4)
 
 def part_2(input):
-  print('Part 2 not implemented yet')
-  return 0
+  return solve(input, 14)
