c0e98de86d6e7490fd5ec11ec20474976c699512
Rik van Toor <rik.van.toor@gmail.com>
2022-12-12 09:20:35 +0100

    Day 12

--- days/day12.py
+++ days/day12.py
@@ -1,12 +1,63 @@
 import utils
+import astar
+
+grid = []
 
 def parse(input):
-  return input
+  cur_pos = (0, 0)
+  goal = (0, 0)
+  ls = input.rstrip().split('\n')
+  for y in range(len(ls)):
+    row = []
+    for x in range(len(ls[y])):
+      c = ls[y][x]
+      if c == 'S':
+        cur_pos = (x, y)
+        row.append(0)
+      elif c == 'E':
+        goal = (x, y)
+        row.append(25)
+      else:
+        row.append(ord(c) - ord('a'))
+    grid.append(row)
+
+  return (grid, cur_pos, goal)
+
+def ok_move(val1, val2):
+  return val2 <= val1 + 1
+
+def get_neighbours(pos, invert=False):
+  (x, y) = pos
+  ds = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+  ns = [(x + dx, y + dy)
+        for dx, dy in ds
+        if 0 <= x + dx < len(grid[0])
+        if 0 <= y + dy < len(grid)
+        if (grid[y+dy][x+dx] - grid[y][x] <= 1
+            if not invert
+            else grid[y+dy][x+dx] - grid[y][x] >= -1)
+       ]
+  return ns
 
 def part_1(input):
-  print('Part 1 not implemented yet')
-  return 0
+  (_, start_pos, goal) = input
+  res = astar.find_path(
+    start_pos,
+    goal,
+    get_neighbours,
+    heuristic_cost_estimate_fnct= lambda a, b: 1
+  )
+  path = list(res)
+  return len(path) - 1
 
 def part_2(input):
-  print('Part 2 not implemented yet')
-  return 0
+  (_, start_pos, goal) = input
+  res = astar.find_path(
+    goal,
+    start_pos,
+    lambda n: get_neighbours(n, True),
+    heuristic_cost_estimate_fnct= lambda a, b: 1,
+    is_goal_reached_fnct=lambda pos_a,pos_b: grid[pos_a[1]][pos_a[0]] == 0,
+  )
+  path = list(res)
+  return len(path) - 1
